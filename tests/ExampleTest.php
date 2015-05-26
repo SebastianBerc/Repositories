76f320f1bc7dd80652a6c44daf7d5ebcecf2d1c8
Sebastian Berć <sebastian.berc@gmail.com>
2015-05-27 01:10:13 +0200

    Add example test.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,19 @@
+<?php namespace SebastianBerc\Repositories\Test;
+
+/**
+ * Class ExampleTest
+ *
+ * @author  Sebastian Berć <sebastian.berc@gmail.com>
+ *
+ * @package SebastianBerc\Repositories\Test
+ */
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
